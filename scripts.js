368cb2d28ff5d5b3d4e4da5ce7b0ab7f9a42ed3f
Matthew Petela <matthew@petela.dev>
2021-04-03 03:52:42 -0400

    Demo site. Needs major futures.

---
+++ scripts.js
@@ -0,0 +1,39 @@
+var timeoutTime = 950;
+var currentEmoji = "&#128293;";
+var lastTrail = performance.now();
+var limitTrail = 20;
+
+function mouseTrail(e){
+    if (limitTrail + lastTrail > performance.now()){
+        return;
+    }
+    lastTrail = performance.now();
+    var htmlElement = document.createElement("div");
+    htmlElement.innerHTML = currentEmoji;
+    htmlElement.className = "emoji-style fade-out";
+    htmlElement.style.left = e.pageX + "px";
+    htmlElement.style.top = e.pageY + "px";
+    htmlElement = document.body.appendChild(htmlElement);
+    setTimeout(function(){
+        htmlElement.remove();
+    }, timeoutTime);
+}
+
+function setLimitTrail(num){
+    limitTrail = num;
+    console.log(limitTrail);
+    document.getElementById("limitTrailValue").innerText = limitTrail;
+}
+
+function setTimeoutTime(num){
+    timeoutTime = num;
+    document.getElementById("timeoutTimeValue").innerText = timeoutTime;
+}
+
+document.getElementById("body").addEventListener("mousemove", e => mouseTrail(e), false);
+document.getElementById("limitTrail").addEventListener("change", e => setLimitTrail(e.target.value), false)
+document.getElementById("timeoutTime").addEventListener("change", e => setTimeoutTime(e.target.value), false)
+document.getElementById("limitTrailValue").innerText = limitTrail;
+document.getElementById("timeoutTimeValue").innerText = timeoutTime;
+
+console.log(limitTrail);
